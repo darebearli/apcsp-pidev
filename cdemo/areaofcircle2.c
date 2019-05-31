f9b71442d4c4babd19390fd4b841a37e491fda6f
darebearli <derek.li2001@gmail.com>
2019-05-31 18:34:07 +0000

    new circle code

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -11,14 +11,20 @@ int main(int argc, char* argv[]){
   float n1;
   float n2;
    char input[256], name[256];
+  if (argc == 1) {
   printf("Type in a radius between 3.5 and 12.5");
   printf("Type 1st radius \n");
   fgets(input, 256, stdin);
    sscanf(input, "%f", &n1);
- printf("Type second radius \n");
+   printf("Type second radius \n");
  fgets(input, 256, stdin);
   sscanf(input, "%f", &n2);
-  
+  }
+  else
+{
+sscanf(argv[1], "%f", &n1);
+sscanf(argv[2], "%f", &n2);
+}
   float n;
   for (n=n1; n<=n2; n++)
   {
